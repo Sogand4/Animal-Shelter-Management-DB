98903210653ccb7c91c5cacbc50c031d25598efd
sogand4 <sogand4@student.ubc.ca>
2023-11-23 18:24:21 -0800

    basic home page setup

--- Pages/index.php
+++ Pages/index.php
@@ -7,12 +7,19 @@ include("../connection.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My website</title>
+	<title>Home</title>
 </head>
 <body>
 	
-    <!-- TODO: SOGAND: HOME PAGE -->
-	<h1>This is the index page. This will be home page later</h1>
+	<h1>Welcome to your Animal Shelter Management System!</h1>
+
+	<h3>If you wish to reset the system press on the reset button on the navigation bar.
+		If this is the first time you're running this page, you MUST use reset button.</h3>
+
+	<h5>Below is some useful information about the shelter you manage: </h5>
+
+	<p>Shelter Name: <?php echo $currShelterName; ?></p>
+	<p>Shelter Location: <?php echo $currShelterLoc; ?></p>
 
 	<br>
 	Hello, welcome.
