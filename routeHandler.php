53a4a6a9fba9a0ec8c2ba6cadbad635562db5332
sogand4 <sogand4@student.ubc.ca>
2023-11-25 16:12:06 -0800

    Added creedit + cleaned up code

--- routeHandler.php
+++ routeHandler.php
@@ -472,16 +472,16 @@
         OCICommit($db_conn);
     }
 
-    function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-        //echo "<br>running ".$cmdstr."<br>";
+    // Function from: https://www.students.cs.ubc.ca/~cs-304/resources/php-oracle-resources/php-setup.html
+    // takes a plain (no bound variables) SQL command and executes it
+    function executePlainSQL($cmdstr) {
         global $db_conn, $success;
 
         $statement = OCIParse($db_conn, $cmdstr);
-        //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
 
         if (!$statement) {
             echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-            $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+            $e = OCI_Error($db_conn);
             echo htmlentities($e['message']);
             $success = False;
         }
@@ -489,7 +489,7 @@
         $r = OCIExecute($statement, OCI_DEFAULT);
         if (!$r) {
             echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-            $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+            $e = oci_error($statement);
             echo htmlentities($e['message']);
             $success = False;
         }
@@ -497,12 +497,9 @@
         return $statement;
     }
 
-    function executeBoundSQL($cmdstr, $list) {
-        /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
-    In this case you don't need to create the statement several times. Bound variables cause a statement to only be
-    parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
-    See the sample code below for how this function is used */
 
+    // Function adapted from: https://www.students.cs.ubc.ca/~cs-304/resources/php-oracle-resources/php-setup.html
+    function executeBoundSQL($cmdstr, $list) {
         global $db_conn, $success;
         $statement = OCIParse($db_conn, $cmdstr);
 
@@ -515,16 +512,14 @@
 
         foreach ($list as $tuple) {
             foreach ($tuple as $bind => $val) {
-                //echo $val;
-                //echo "<br>".$bind."<br>";
                 OCIBindByName($statement, $bind, $val);
-                unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+                unset ($val);
             }
 
             $r = OCIExecute($statement, OCI_DEFAULT);
             if (!$r) {
                 echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-                $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
+                $e = OCI_Error($statement);
                 echo htmlentities($e['message']);
                 echo "<br>";
                 $success = False;
